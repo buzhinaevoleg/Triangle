164f2b1477aa80a94144378f44ec51722933e38a
buzhinaevoleg <159452698+buzhinaevoleg@users.noreply.github.com>
2025-02-20 00:02:35 +0800

    Update Test.cpp

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,54 +1,65 @@
-﻿#define GLEW_DLL
+// lr1_buzhi.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define GLEW_DLL
 #define GLFW_DLL
 
-#include <stdio.h>
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
+#include <cstdio>
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
 
 int main()
 {
-	if (!glfwInit()) {
-		fprintf(stderr, "ERROR: could not start GLFW3\n");
-	}
+    printf("Hello World!\n");
+    glfwInit();
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR:could not start GLFW3. \n");
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE );
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window;
+    window = glfwCreateWindow(512, 512, "Mainwindow", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "lalala", NULL, NULL);
-	if (!window) {
-		fprintf(stderr, "ERROR: could not create window\n");
-		glfwTerminate();
-		return -1;
-	}
+    glfwMakeContextCurrent(window);
 
-	glfwMakeContextCurrent(window);
-	glewExperimental = GL_TRUE;
+    GLenum ret = glewInit();
+    if (GLEW_OK != ret) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(ret));
+        return 1;
+    }
 
-	GLenum ret = glewInit();
-	if (GLEW_OK != ret) {
-		fprintf(stderr, "ERROR", glewGetErrorString(ret));
-		return 1;
-	}
-
-	while (!glfwWindowShouldClose(window)) {
-		glClearColor(0.5, 0.5, 0.5, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBegin(GL_TRIANGLES);
-		glColor3f(0.0, 1.0, 0.0);
-		glVertex2f(0.0, 0.5);
-		glColor3f(1.0, 0.0, 0.0);
-		glVertex2f(-0.5, -0.5);
-		glColor3f(0.0, 0.0, 1.0);
-		glVertex2f(0.5, -0.5);
-		glEnd();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
-	return 0;
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.5, 0.5, 0.5, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex2f(0.0, 0.5);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f(-0.5, -0.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(0.5, -0.5);
+        glEnd();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate;
 }
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
